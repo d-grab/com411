6afa085d0fac460b7e4b30677eb5476bdf82ab58
repl.it user <replituser@example.com>
2021-03-06 15:35:55 +0000

    Added code to demonstrate t counting down with a for loop

---
+++ basics/repetisions/for_loop/count_down.py
@@ -0,0 +1,7 @@
+print ("How far we are from the cave ?")
+distance=int (input())
+
+for distances in range(distance,0,-1):
+  print(distances,"steps remaining")
+
+print ("We have reached the cave!")
