75d8b8f7ee291cb1b11c89dd6a8ab582a6e622b4
repl.it user <replituser@example.com>
2021-03-06 15:18:24 +0000

    Added code to demonstrate the simple use of for loop

---
+++ basics/repetisions/for_loop/simple.py
@@ -0,0 +1,12 @@
+print ("How many mountsins should I dispay ?")
+mountain=int (input())
+
+print ("Displaying...")
+for mountains in range (mountain) :
+  
+  print("""___
+          /  \\_  
+         /^    \\
+        /  ^    \\_
+      _/ ^ ^     ^\\
+     /  ^     ^    \\""")
