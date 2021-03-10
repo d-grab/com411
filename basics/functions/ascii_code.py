a6968fbccee117c978a7e5edf0002563bfe1aa6f
repl.it user <replituser@example.com>
2021-03-10 13:44:48 +0000

    Added code to demonstrate the use of built-in functions: len,ord

---
+++ basics/functions/ascii_code.py
@@ -0,0 +1,10 @@
+print("Program started")
+print("Please enter a standard character")
+char=input()
+
+if 1==len(char) :
+  print(f"The ascii code for leter '{char}' is :",ord(char))
+else:
+  print("It has to be one letter character ")
+print("\nProgram Ended !")
+
