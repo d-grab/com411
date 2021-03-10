9eeb56ded6438b7b198c9c11788ff123290d5dcd
repl.it user <replituser@example.com>
2021-03-10 14:25:04 +0000

    Added code to demonstrate the use of defined-user function with nested decisions

---
+++ basics/functions/function_with_nesting.py
@@ -0,0 +1,12 @@
+#Define function
+def identify ():
+  print("What lies before us ?")
+  response=input()
+ #Display message 
+  if response=="a large boudler" :
+    print("It's time to run!")
+  else:
+    print("We will be fine ")
+
+#Callign the function
+identify()
